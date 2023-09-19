443966eecae4063e74b7d4f23b9be8ff98ab41e0
bass <gtluu912@gmail.com>
2023-09-19 15:24:33 -0700

    first

---
+++ README.md
@@ -0,0 +1,15 @@
+# pyBaf2Sql
+
+## About
+This package is a Python wrapper for Bruker's Baf2Sql data access library to be used with other Python packages.
+
+## Installation
+This package can be installed to a Python virtual environment using `pip`. 
+```
+pip install git+https://github.com/gtluu/pyBaf2Sql
+```
+
+## Example Usage
+```python
+
+```
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='pyBaf2Sql',
+      version='0.1.0',
+      description='Python wrapper for Bruker Baf2Sql',
+      url='https://github.com/gtluu/pyBaf2Sql',
+      author='Gordon T. Luu',
+      author_email='gtluu912@gmail.com',
+      license='Apache License',
+      packages=['pyBaf2Sql', 'Baf2Sql'],
+      include_package_data=True,
+      package_data={'': ['*.dll', '*.so']},
+      install_requires=['numpy', 'pandas'])
