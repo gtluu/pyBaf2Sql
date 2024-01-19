8ff77e44dbe6de404f6401d7a4668743bcd8e572
bass <gtluu912@gmail.com>
2024-01-19 08:17:39 -0800

    update documentation

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -11,9 +11,9 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..',
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'pyBaf2Sql'
-copyright = '2023, Gordon T. Luu'
+copyright = '2024, Gordon T. Luu'
 author = 'Gordon T. Luu'
-release = '0.1.0'
+release = '0.2.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
